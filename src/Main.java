548d0cf1493eecebeba236c67b730df69559a5a9
Silantev Artem <silantev.artem@yandex.ru>
2025-02-24 23:34:56 +0300

    Домашнее задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 ////inside the project with the code you don't need to write any code
-
+/// the main thing is to consolidate the process of completing homework.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");

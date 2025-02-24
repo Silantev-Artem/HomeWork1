3a03c9426ef5b89b5054f622a41195f5981c4f98
Silantev Artem <silantev.artem@yandex.ru>
2025-02-24 23:29:52 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+////inside the project with the code you don't need to write any code
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
